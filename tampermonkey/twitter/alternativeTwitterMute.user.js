af4025aa0764f21d295ad222869a69fe3e89b628
HBIDamian <10010369+HBIDamian@users.noreply.github.com>
2022-05-20 21:46:32 +0100

    Sometimes, twitter can go fuck it's self sideways!
    Sometimes, twitter can go fuck it's self sideways!

---
+++ tampermonkey/twitter/alternativeTwitterMute.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Alternative Twitter Mute
+// @namespace    alternativeTwitterMute.user.js
+// @version      1.0.0
+// @description  try to take over the world!
+// @author       HBIDamian
+// @updateURL    https://github.com/IdioticBuffoonery/Browser-Styles-and-Scripts/raw/main/tampermonkey/twitter/alternativeTwitterMute.user.js
+// @downloadURL  https://github.com/IdioticBuffoonery/Browser-Styles-and-Scripts/raw/main/tampermonkey/twitter/alternativeTwitterMute.user.js
+// @match        *://*.twitter.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const phrasesToRemove = [
+        'Chris Rock',
+        'Will Smith',
+        'Elon Musk',
+        'elonmusk',
+        'Amber Heard',
+        'Johnny Depp',
+        'AmberHeard',
+        'JohnnyDepp'
+    ];
+    takeOutTheTrash();
+    setInterval(takeOutTheTrash, 1000);
+
+    function takeOutTheTrash(){
+        var whatsHappening = document.querySelectorAll('div[class="css-1dbjc4n r-1adg3ll r-1ny4l3l"]');
+        var tweets = document.querySelectorAll('article[data-testid="tweet"]');
+        var notifications = document.querySelectorAll('div[aria-label="Timeline: Notifications"] div div[class="css-1dbjc4n r-1igl3o0 r-qklmqi r-1adg3ll r-1ny4l3l"]');
+        for (let i = 0; i < phrasesToRemove.length; ++i) {
+            Array.from(whatsHappening)
+                .filter(v => v.textContent.includes(phrasesToRemove[i]))
+                .forEach(a=> a.remove());
+            Array.from(tweets)
+                .filter(v => v.textContent.includes(phrasesToRemove[i]))
+                .forEach(a=> a.parentElement.parentElement.parentElement.remove());
+            Array.from(notifications)
+                .filter(v => v.textContent.includes(phrasesToRemove[i]))
+                .forEach(a=> a.remove());
+        }
+    }
+})();
