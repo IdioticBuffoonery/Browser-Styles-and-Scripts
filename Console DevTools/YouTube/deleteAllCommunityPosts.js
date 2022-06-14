0af50ad67cb83c70fb880569a5e6f0eacf80e325
HBIDamian <10010369+HBIDamian@users.noreply.github.com>
2022-06-14 12:37:23 +0100

    Add files via upload

---
+++ Console DevTools/YouTube/deleteAllCommunityPosts.js
@@ -0,0 +1,31 @@
+// Based on https://webapps.stackexchange.com/a/137482 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function deleteLikedVideos() {
+    'use strict';
+    var items = document.querySelectorAll('ytd-menu-renderer yt-icon-button.dropdown-trigger > button[aria-label="Action menu"]');
+    var out;
+    var out2;
+
+    for (var i = 0; i < items.length; i++) {
+        items[i].click();
+        out = setTimeout(function () {
+            if (document.querySelector('tp-yt-paper-listbox .ytd-menu-navigation-item-renderer').lastElementChild) {
+                document.querySelector('tp-yt-paper-listbox .ytd-menu-navigation-item-renderer').lastElementChild.click();
+            }
+        }, 100);
+        out2 = setTimeout(function () {
+            if (document.querySelector('tp-yt-paper-button[aria-label="Delete"]')) {
+                document.querySelector('tp-yt-paper-button[aria-label="Delete"]').click();
+            }
+        }, 250);
+
+        await sleep(500); // sleep cause browser can not handle the process
+        clearTimeout(out);
+        clearTimeout(out2);
+    }
+}
+
+deleteLikedVideos();
